c070b92a1f3cf91bb94ae00202f046a9758092ee
PiyushChauhan07 <piyushchouhan240701@gmail.com>
2022-06-16 23:30:57 +0530

    Modified Landing Page

--- controllers/room.js
+++ controllers/room.js
@@ -3,6 +3,7 @@ const idGenerator=require('nodejs-unique-numeric-id-generator');
 // Sending Room Id to admin so that admin can create room at Server level
 module.exports.createRoom=(req,res)=>{
     const { numpi, botallowed }=req.body;
+    console.log(req.body);
     
     // It will give me 6 digit unique id
     const roomid=idGenerator.generate(new Date().toJSON());
--- routes/room.js
+++ routes/room.js
@@ -1,9 +1,9 @@
 const express= require('express');
 const Router=express.Router();
 const { createRoom }=require('../controllers/room')
-
+const {checkAuth} = require('../config/middleware')
 // @Route: /room/create  for creating a room
-Router.post('/create', createRoom)
+Router.post('/create', checkAuth, createRoom)
 
 
 
